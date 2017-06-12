acfe8f77a393710b8c59cb9ee711f1dc43a90536
Ryan Olson <rolson@nvidia.com>
2017-06-12 14:52:45 -0600

    updated to use /datasets; renamed run script

--- run-train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nvidia-docker run --rm -ti -v $PWD:/source  nv/pix2pix-torch:devel ./train.sh
--- train.sh
+++ train.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 ROOT="${ROOT:-/source}"
 DATASET="${DATASET:-facades}"
-DATA_ROOT="${DATA_ROOT:-$ROOT/datasets/$DATASET}"
+DATA_ROOT="${DATA_ROOT:-/datasets/$DATASET}"
 DATA_ROOT=$DATA_ROOT name="${DATASET}_generation" which_direction=BtoA th train.lua
-
